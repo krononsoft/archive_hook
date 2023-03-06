d332617efecf9c20406a283a9fe92f884a247016
alvir <shurik.v.r@gmail.com>
2023-03-06 13:32:29 +0300

    Initial gem files.

---
+++ lib/archive_hook.rb
@@ -0,0 +1,6 @@
+require "archive_hook/version"
+
+module ArchiveHook
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/generators/archive_hook_generator.rb
@@ -0,0 +1,9 @@
+require 'rails/generators/active_record'
+
+module ActiveRecord
+  module Generators
+    class ArchiveHookGenerator < ActiveRecord::Generators::Base
+
+    end
+  end
+end

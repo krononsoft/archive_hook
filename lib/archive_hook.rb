eecedd8e842396012714e41faad720146f8d6c82
alvir <shurik.v.r@gmail.com>
2023-04-10 13:54:44 +0300

    Add archiver.

--- lib/archive_hook.rb
+++ lib/archive_hook.rb
@@ -2,5 +2,48 @@ require "archive_hook/version"
 
 module ArchiveHook
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Archiver
+    def initialize(dependencies: {}, archive_date:)
+      @dependencies = dependencies
+      @archive_date = archive_date
+    end
+
+    def call(scope)
+      parent = scope.model
+      if @dependencies[parent].present?
+        parent_ids = scope.or(expiration_scope(parent)).pluck(:id)
+        @dependencies[parent].each do |child|
+          call(child.where(parent.to_s.foreign_key => parent_ids))
+        end
+      end
+      archive_by_scope(scope.or(expiration_scope(parent)))
+    end
+
+    private
+
+    def archive_by_scope(scope)
+      ActiveRecord::Base.connection.execute(Arel.sql(archive_records_sql(scope)))
+      scope.delete_all
+    end
+
+    def archive_records_sql(scope)
+      table_name = scope.table_name
+      attributes_list = scope.column_names.join(",")
+      <<-SQL
+        INSERT INTO #{table_name}_archive (#{attributes_list})
+        #{scope.select(attributes_list).to_sql}
+      SQL
+    end
+
+    def expiration_scope(model)
+      model.where("created_at < ?", @archive_date)
+    end
+  end
+
+  class << self
+    def archive(root, archive_date, dependencies)
+      Archiver.new(dependencies: dependencies, archive_date: archive_date).call(root.none)
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require "bundler/setup"
 require "archive_hook"
 
+RSpec::Matchers.define_negated_matcher :not_change, :change
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"

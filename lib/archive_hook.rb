90f3a5a037348b27b710176e1b0369cb57f5aa03
alvir <shurik.v.r@gmail.com>
2023-04-16 23:45:22 +0300

    Extract scope archiver to use it independently.

--- lib/archive_hook.rb
+++ lib/archive_hook.rb
@@ -32,17 +32,7 @@ module ArchiveHook
     private
 
     def archive_by_scope(scope)
-      ActiveRecord::Base.connection.execute(Arel.sql(archive_records_sql(scope)))
-      scope.delete_all
-    end
-
-    def archive_records_sql(scope)
-      table_name = scope.table_name
-      attributes_list = scope.column_names.join(",")
-      <<-SQL
-        INSERT INTO #{table_name}_archive (#{attributes_list})
-        #{scope.select(attributes_list).to_sql}
-      SQL
+      scope_archiver.call(scope)
     end
 
     def expiration_scope(model)
@@ -53,11 +43,42 @@ module ArchiveHook
         model.where("created_at < ?", @archive_date)
       end
     end
+
+    def scope_archiver
+      @scope_archiver ||= ScopeArchiver.new(dependencies: @dependencies, archive_date: @archive_date)
+    end
+  end
+
+  class ScopeArchiver
+    def initialize(dependencies: {}, archive_date:)
+      @dependencies = dependencies
+      @archive_date = archive_date
+    end
+
+    def call(scope)
+      ActiveRecord::Base.connection.execute(Arel.sql(archive_records_sql(scope)))
+      scope.delete_all
+    end
+
+    private
+
+    def archive_records_sql(scope)
+      table_name = scope.table_name
+      attributes_list = scope.column_names.join(",")
+      <<-SQL
+        INSERT INTO #{table_name}_archive (#{attributes_list})
+        #{scope.select(attributes_list).to_sql}
+      SQL
+    end
   end
 
   class << self
     def archive(root, archive_date, dependencies)
       Archiver.new(dependencies: dependencies, archive_date: archive_date).call(root.none)
+    end
+
+    def archive_scope(scope, archive_date, dependencies = {})
+      ScopeArchiver.new(dependencies: dependencies, archive_date: archive_date).call(scope)
     end
   end
 end

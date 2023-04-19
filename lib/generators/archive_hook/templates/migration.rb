13dfe874cf42349a230a94546a7eb3c63fa8c5a4
Alexander Ryazantsev <shurik.v.r@gmail.com>
2023-04-19 14:05:15 +0300

    Minor cleanup.

--- lib/generators/archive_hook/templates/migration.rb
+++ lib/generators/archive_hook/templates/migration.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ArchiveHookCreate<%= archive_table_name.camelize %> < ActiveRecord::Migration<%= migration_version %>
+class Create<%= archive_table_name.camelize %> < ActiveRecord::Migration<%= migration_version %>
   def up
     execute("CREATE TABLE <%= archive_table_name %> (LIKE <%= table_name %>)")
   end
